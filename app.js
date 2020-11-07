8912213eb58871c1b7201f418cf37ce4eaeafdc7
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2020-11-07 16:44:29 +0100

    1.0.0

--- app.js
+++ app.js
@@ -7,6 +7,31 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var oreID = require('oreid-auth');
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { 
+  OREID_APP_ID, 
+  OREID_API_KEY, 
+  OREID_SERVICE_KEY, 
+  OREID_URL 
+} = process.env;
+
+const { 
+  AUTH_CALLBACK, 
+  SIGN_CALLBACK, 
+  BACKGROUND_COLOR 
+} = process.env;
+
+auth = oreID.init({
+  appID: OREID_APP_ID,
+  ApiKey: OREID_API_KEY
+  ServiceKey: OREID_SERVICE_KEY
+  url: OREID_URL
+});
+
 var app = express();
 
 // view engine setup
@@ -19,8 +44,46 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', helpPrompt());
+
+//handle sample oreid-enabled routes
+app.use('/login/:logintype', loginHandler(auth.oreid));
+app.use('/authcallback', auth.authCallback, displayUser());
+app.use('/signcallback', auth.signCallback);
+
+let loginHandler = (oreId) => {
+  return asyncHandler(async function(req, res, next) {
+    const provider = req.params.logintype;
+    let authUrl = await oreId.getOreIdAuthUrl({ provider, callbackUrl: AUTH_CALLBACK, signCallbackUrl: SIGN_CALLBACK, backgroundColor: BACKGROUND_COLOR });
+    //redirect browser
+    res.redirect(authUrl);
+  });
+
+};
+
+//display user state
+function displayUser() {
+  return function(req, res, next) {
+    const { user } = req;
+    if (user) {
+      return res.status(200).json(user);
+    }
+  };
+}
+
+function helpPrompt() {
+  return function(req, res, next) {
+    let message = 'Start login flow using /login route. Try it like this: {http://yourserver:port}/login/facebook';
+    return res.status(200).send(message);
+  };
+}
+
+//Generic async handler for Express Middleware
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+// ---------------------------------------------------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+//Load settings
+const { 
+        AUTH_CALLBACK, 
+        SIGN_CALLBACK, 
+        BACKGROUND_COLOR 
+      } = process.env;
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login/:logintype', function(req, res, next) {
+
+  //redirect browser to OAuth flow
+function loginHandler(oreId) {
+  return asyncHandler(async function(req, res, next) {
+    const provider = req.params.logintype;
+    let authUrl = await oreId.getOreIdAuthUrl({ provider, callbackUrl: AUTH_CALLBACK, signCallbackUrl: SIGN_CALLBACK, backgroundColor: BACKGROUND_COLOR });
+    //redirect browser
+    res.redirect(authUrl);
+  });
+}
+
 });
+
+app.use('/login/:logintype', usersRouter);
+app.use('/authcallback', auth.authCallback, usersRouter);
+app.use('/signcallback', auth.signCallback);
+
+//Generic async handler for Express Middleware
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
 
 module.exports = router;
